df6270dedd2941c0b9165a0757697b1005da6326
reymart0132 <reymart.bolasoc@gmail.com>
2023-04-27 12:27:30 +0800

    Update release.php

--- vendor/release.php
+++ vendor/release.php
@@ -54,8 +54,8 @@ try {
     $mail->isSMTP();
     $mail->Host       = 'smtp.gmail.com';     //platform
     $mail->SMTPAuth   = true;
-     $mail->Username   = getEm();   //email
-    $mail->Password   = getToken();                   //password
+    $mail->Username   = 'ceuourmailerMNL@gmail.com';   //email
+    $mail->Password   = 'acyucxtqbzbatyrg';                   //password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port       = 587;
     $mail->CharSet = 'UTF-8';
